9d305ccd1e22858ed0c2449d2a9f2855b58eba11
Ksawery <ksaweryjackiewicz759@gmail.com>
2026-02-17 09:53:08 +0100

    dsdsds

--- app/src/main/java/com/example/ekh2026/MainActivity.kt
+++ app/src/main/java/com/example/ekh2026/MainActivity.kt
@@ -37,6 +37,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+@Composable
+fun Button() {
+    Button()
+}
 
 @Preview(showBackground = true)
 @Composable
